ece97cd43a477c346cf9f892395991bfbe5c9314
Swhag <khg5293@outlook.com>
2022-12-10 20:37:08 -0500

    style sheet added

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,6 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
+import '../styles/App.css';
+
 class App extends Component {
   render() {
     return (
